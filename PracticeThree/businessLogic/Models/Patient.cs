cf5723ba9144d6f1a37066dab1490f29dd628adb
Alejandro Uriarte <alejandro.uriartepatsi@gmail.com>
2024-05-13 15:23:32 -0400

    Added project practiceTwo and created practiceThree

---
+++ PracticeThree/businessLogic/Models/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace businessLogic.Models
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int CI { get; set; }
+
+        public Patient(string name, string lastname, int ci)
+        {
+            Name = name;
+            Lastname = lastname;
+            CI = ci;
+        }
+
+    }
+}
